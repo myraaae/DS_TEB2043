6ce43c80f502ae4710970cdf4e90be62bd8627ef
myraaae <airahazhar@gmail.com>
2026-02-25 23:28:20 +0800

    Create Task-1.R

---
+++ 2 LAB 2B - 03.02.2026/Task-1.R
@@ -0,0 +1,19 @@
+#get input
+weight <- as.numeric(readline("Weight (kg): "))
+height <- as.numeric(readline("Height (m): "))
+
+#BMI calculation & display
+bmi <- weight / (height^2)
+cat("Your BMI is ", bmi, "\n\n")
+
+#categories
+underweight <- bmi <= 18.4
+normal <- bmi >= 18.5 & bmi <= 24.9
+overweight <- bmi >= 25.0 & bmi <= 39.9
+obese <- bmi >= 40.0
+
+# Display results
+cat("Underweight:", underweight, "\n")
+cat("Normal:", normal, "\n")
+cat("Overweight:", overweight, "\n")
+cat("Obese:", obese, "\n")
